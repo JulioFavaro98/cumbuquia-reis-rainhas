6e998556dcb53c77a8812ec720fc0a158c3f8ae8
JulioFavaro98 <juliofv66@gmail.com>
2024-07-20 18:11:19 -0300

    Primeiro commit

--- lib/desafio_cli.ex
+++ lib/desafio_cli.ex
@@ -1,17 +1,54 @@
 defmodule DesafioCli do
-  @moduledoc """
-  Ponto de entrada para a CLI.
-  """
 
-  @doc """
-  A função main recebe os argumentos passados na linha de
-  comando como lista de strings e executa a CLI.
-  """
   def main(_args) do
-    IO.puts(phrase())
+    IO.puts("***************************************<<<<<<<<<<>>>>>>>>>>***************************************")
+    IO.puts("________________________________________________________________________________________________")
+    IO.puts("Ferramenta de numeração de reis e rainhas de Cumbúquia")
+    IO.puts("Digite os nomes dos reis e rainhas, um por linha. Insira uma linha em branco para finalizar.")
+    IO.puts("________________________________________________________________________________________________")
+    IO.puts("***************************************<<<<<<<<<<>>>>>>>>>>***************************************")
+
+    nomes = ler_nomes([])
+    {resultado, _} = Enum.map_reduce(nomes, %{}, fn nome, contagem_nomes ->
+      {nome_atualizado, nova_contagem_nomes} = incrementar_contagem(nome, contagem_nomes)
+      {nome_atualizado, nova_contagem_nomes}
+    end)
+
+    IO.puts("Lista de reis e rainhas com numeração:")
+    Enum.each(resultado, &IO.puts/1)
   end
 
-  def phrase() do
-    "Hello, world!"
+  defp ler_nomes(acumulador) do
+    nome = IO.gets("")
+    case nome do
+      :eof -> acumulador
+      _ ->
+        nome = String.trim(nome)
+        if nome == "" do
+          acumulador
+        else
+          ler_nomes(acumulador ++ [nome])
+        end
+    end
+  end
+
+  defp incrementar_contagem(nome, contagem_nomes) do
+    contagem = Map.get(contagem_nomes, nome, 0) + 1
+    nome_atualizado = "#{nome} #{int_to_roman(contagem)}"
+    {nome_atualizado, Map.put(contagem_nomes, nome, contagem)}
+  end
+
+  defp int_to_roman(num) do
+    val = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
+    syb = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"]
+    build_roman(num, val, syb, "")
+  end
+
+  defp build_roman(0, _, _, roman_num), do: roman_num
+  defp build_roman(num, [v | val_tail], [s | syb_tail], roman_num) when num >= v do
+    build_roman(num - v, [v | val_tail], [s | syb_tail], roman_num <> s)
+  end
+  defp build_roman(num, [_ | val_tail], [_ | syb_tail], roman_num) do
+    build_roman(num, val_tail, syb_tail, roman_num)
   end
 end
--- test/desafio_cli_test.exs
+++ test/desafio_cli_test.exs
@@ -1,8 +1,76 @@
 defmodule DesafioCliTest do
   use ExUnit.Case
-  doctest DesafioCli
+  import ExUnit.CaptureIO
 
-  test "greets the world" do
-    assert DesafioCli.phrase() == "Hello, world!"
+  alias DesafioCli
+
+  test "processa lista de nomes com numeração correta" do
+    nomes_input = """
+    Eduardo
+    Maria
+    Flavia
+    Diana
+    Eduardo
+    Diana
+    Flavia
+    Joao
+    Joao
+    Joao
+    """
+
+    expected_output = """
+    ***************************************<<<<<<<<<<>>>>>>>>>>***************************************
+    ________________________________________________________________________________________________
+    Ferramenta de numeração de reis e rainhas de Cumbúquia
+    Digite os nomes dos reis e rainhas, um por linha. Insira uma linha em branco para finalizar.
+    ________________________________________________________________________________________________
+    ***************************************<<<<<<<<<<>>>>>>>>>>***************************************
+    Lista de reis e rainhas com numeração:
+    Eduardo I
+    Maria I
+    Flavia I
+    Diana I
+    Eduardo II
+    Diana II
+    Flavia II
+    Joao I
+    Joao II
+    Joao III
+    """
+
+    output = capture_io([input: nomes_input], fn ->
+      DesafioCli.main([])
+    end)
+
+    assert normalize_output(output) == normalize_output(expected_output)
+  end
+
+  test "processa lista de nomes vazia corretamente" do
+    nomes_input = """
+
+    """
+
+    expected_output = """
+    ***************************************<<<<<<<<<<>>>>>>>>>>***************************************
+    ________________________________________________________________________________________________
+    Ferramenta de numeração de reis e rainhas de Cumbúquia
+    Digite os nomes dos reis e rainhas, um por linha. Insira uma linha em branco para finalizar.
+    ________________________________________________________________________________________________
+    ***************************************<<<<<<<<<<>>>>>>>>>>***************************************
+    Lista de reis e rainhas com numeração:
+    """
+
+    output = capture_io([input: nomes_input], fn ->
+      DesafioCli.main([])
+    end)
+
+    assert normalize_output(output) == normalize_output(expected_output)
+  end
+
+
+  defp normalize_output(output) do
+    output
+    |> String.replace("\r\n", "\n")
+    |> String.trim()
   end
 end
